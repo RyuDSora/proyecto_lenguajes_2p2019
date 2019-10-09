e54067bdef8465eaa410c9cf5df863a643db8e82
RyuDSora <47126608+RyuDSora@users.noreply.github.com>
2019-10-08 20:36:57 -0600

    Add files via upload

--- lex.yy.c
+++ lex.yy.c
@@ -282,12 +282,12 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
-static yyconst short int yy_accept[16] =
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
+static yyconst short int yy_accept[12] =
     {   0,
-        0,    0,    5,    4,    4,    3,    2,    2,    3,    2,
-        2,    2,    2,    1,    0
+        0,    0,    4,    3,    1,    1,    2,    1,    1,    2,
+        0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -295,16 +295,16 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    2,    1,    1,    3,    3,    3,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
         3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
         1,    1,    1,    1,    4,    4,    4,    4,    4,    4,
         4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
-        4,    4,    5,    4,    4,    4,    4,    4,    4,    4,
-        1,    1,    1,    1,    1,    1,    6,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        1,    1,    1,    1,    4,    1,    4,    4,    4,    4,
 
-        4,    4,    4,    4,    7,    4,    4,    8,    4,    4,
-        4,    4,    4,    9,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
         4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -322,35 +322,33 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[10] =
+static yyconst int yy_meta[5] =
     {   0,
-        1,    1,    1,    2,    2,    2,    2,    2,    2
+        1,    2,    3,    4
     } ;
 
-static yyconst short int yy_base[17] =
+static yyconst short int yy_base[14] =
     {   0,
-        0,    0,   18,   19,   14,   13,    0,    9,   11,    0,
-        5,    5,    2,    0,   19,    8
+        0,    0,   11,   13,    3,    0,    0,    0,    0,    0,
+       13,    6,    8
     } ;
 
-static yyconst short int yy_def[17] =
+static yyconst short int yy_def[14] =
     {   0,
-       15,    1,   15,   15,   15,   15,   16,   16,   15,   16,
-       16,   16,   16,   16,    0,   15
+       11,    1,   11,   11,   11,   12,   13,    5,   12,   13,
+        0,   11,   11
     } ;
 
-static yyconst short int yy_nxt[29] =
+static yyconst short int yy_nxt[18] =
     {   0,
-        4,    5,    6,    7,    8,    7,    7,    7,    7,   10,
-       14,   13,   12,    9,   11,    9,    9,   15,    3,   15,
-       15,   15,   15,   15,   15,   15,   15,   15
+        4,    5,    6,    7,    8,    9,   10,    9,    9,   10,
+       11,   10,    3,   11,   11,   11,   11
     } ;
 
-static yyconst short int yy_chk[29] =
+static yyconst short int yy_chk[18] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,   16,
-       13,   12,   11,    9,    8,    6,    5,    3,   15,   15,
-       15,   15,   15,   15,   15,   15,   15,   15
+        1,    1,    1,    1,    5,    5,    5,   12,   12,   13,
+        3,   13,   11,   11,   11,   11,   11
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -364,13 +362,11 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "palabra.l"
+#line 1 "lexico.lex"
 #define INITIAL 0
-#line 2 "palabra.l"
-    #include <stdio.h>
-    int palabra=0;
-    int numero=0;
-#line 374 "lex.yy.c"
+#line 14 "lexico.lex"
+#include <stdio.h>
+#line 370 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -521,9 +517,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 10 "palabra.l"
+#line 17 "lexico.lex"
 
-#line 527 "lex.yy.c"
+#line 523 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -574,13 +570,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 16 )
+				if ( yy_current_state >= 12 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 19 );
+		while ( yy_base[yy_current_state] != 13 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -608,32 +604,20 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 11 "palabra.l"
-{   printf("Numeros: %d,numero");
-				printf("Palabras: %d,palabra");
-                return 0;
-            }
+#line 18 "lexico.lex"
+{printf("Entero %s (%d) \n", yylex,atoi(yytext));}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 15 "palabra.l"
-{printf("Se leyo palabra: %s\n",yytext);
-            palabra++;
-            }
+#line 19 "lexico.lex"
+{printf("palabra %s (%c) \n", yylex,yytext);}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 18 "palabra.l"
-{printf("Se leyo un numero: %d\n",atoi(yytext));
-            numero++;
-            }
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 21 "palabra.l"
+#line 20 "lexico.lex"
 ECHO;
 	YY_BREAK
-#line 637 "lex.yy.c"
+#line 621 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -925,7 +909,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 16 )
+			if ( yy_current_state >= 12 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -960,11 +944,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 16 )
+		if ( yy_current_state >= 12 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 15);
+	yy_is_jam = (yy_current_state == 11);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1519,9 +1503,12 @@ int main()
 	return 0;
 	}
 #endif
-#line 21 "palabra.l"
+#line 20 "lexico.lex"
 
-int main(){
-    printf("Escriba un texto: ");
-    yylex();
+
+main()
+{ 
+yylex();
 }
+
+
